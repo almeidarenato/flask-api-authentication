c0f9a4f30983e9b9f7ddaa3d472d833d634df17d
Renato Almeida <renatoam@ymail.com>
2024-02-13 00:23:05 -0300

    feat: add delete user

--- app.py
+++ app.py
@@ -81,6 +81,21 @@ def update_user(id_user):
         return jsonify({"message":f"Usuário {id_user} atualizado com sucesso"}), 200
     return jsonify({"message":"Usuário não encontrado"}), 404
 
+@app.route("/user/<int:id_user>",methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if id_user == current_user.id:
+        return jsonify({"message": "Deleção não permitida ( o usuario logado está tentando deletar o próprio usuário)"}), 403
+    
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message":f"Usuário {id_user} deletado com sucesso"}), 200
+    
+    return jsonify({"message":"Usuário não encontrado"}), 404
+
 @app.route("/hello-world")
 def hello_world():
     return "Hello World"
